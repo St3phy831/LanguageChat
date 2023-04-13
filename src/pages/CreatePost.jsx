8bbf897218be0ad5e2e4a7d29047f2d3c15a55dc
Stephanie Hernandez <stephernandez831@gmail.com>
2023-04-12 23:22:27 -0700

    Updated css

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,6 +1,7 @@
 import { supabase } from "../client";
 import { useState } from "react";
 import Form from "../components/Form";
+import "./CreatePost.css";
 
 const CreatePost = () => {
   const [post, setPost] = useState({
