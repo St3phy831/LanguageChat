aafc327a262d4a5b3aee4316312172cb3d3584ce
Stephanie Hernandez <stephernandez831@gmail.com>
2023-04-12 22:00:45 -0700

    Created Form

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,7 +1,11 @@
+import Form from "../components/Form";
+
 const CreatePost = () => {
   return (
     <div className="CreatePostForm">
       <h1>Create a Post</h1>
+      <Form />
+      <button>Submit</button>
     </div>
   );
 };
