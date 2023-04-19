0f6eafcbb96cfdb36fb7d3e25d2a8a7f6f25c84f
Stephanie Hernandez <stephernandez831@gmail.com>
2023-04-18 19:55:18 -0700

    Updated Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 const Home = () => {
   return (
     <div className="Home">
-      <h1>Hello! Hola! Konnichiwa! Bonjour! 你好! This is <span className="name">LanguageChat.</span></h1>
+      <h1>
+        Hello! Hola! Konnichiwa! Bonjour! 你好! This is{" "}
+        <span className="name">LanguageChat.</span>
+      </h1>
+      <br></br>
+      <h3 className="center">
+        This is an online forum to post your comments, questions, and knowledge
+        about languages.
+      </h3>
     </div>
   );
 };
